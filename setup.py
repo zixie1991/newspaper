419cb6d2bc1dda2bc3cb979c3b870aadaab823e6
zixie1991 <zixie1991@163.com>
2015-12-09 09:07:18 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Lucas Ou 2014 -- http://lucasou.com
+Setup guide: http://guide.python-distribute.org/creation.html
+python setup.py sdist bdist_wininst upload
+"""
+import sys
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')  # bdist_wininst
+    sys.exit()
+
+
+packages = [
+    'newspaper',
+]
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setup(
+    name='newspaper',
+    version='0.0.9.8',
+    description='Simplified python article discovery & extraction.',
+    author='Lucas Ou-Yang, zixie1991',
+    author_email='lucasyangpersonal@gmail.com, zixie1991@gmail.com',
+    url='https://github.com/zixie1991/newspaper/',
+    packages=packages,
+    include_package_data=True,
+    install_requires=required,
+    license='MIT',
+    zip_safe=False,
+)
